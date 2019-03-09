e19681fc3698e067417668ef5d6ef36b328e2c8e
ompandey <ompandeyinfy@gmail.com>
2019-03-09 12:01:38 +0100

    [SLING-8272] Fixed PR Comments

--- src/main/java/org/apache/sling/caconfig/ConfigurationBuilder.java
+++ src/main/java/org/apache/sling/caconfig/ConfigurationBuilder.java
@@ -87,7 +87,12 @@ public interface ConfigurationBuilder {
     @NotNull <T> Collection<T> asAdaptableCollection(@NotNull Class<T> clazz);
 
     /**
-     * Check if the configuration node is existing
+     * Check if the configuration node is existing in the JCR. This method checks for the configuration existence based on {@code configName} defined in the
+     * configuration definition. It checks if the configuration is available for the context path in the current resource hierarchy and if not found, also
+     * checks in the global fall-back paths configured via {@link org.apache.sling.caconfig.resource.impl.def.DefaultConfigurationResourceResolvingStrategy}
+     * which by default are {@code /conf/global}, {@code /apps/config} and {@code /libs/config}.This method does not consider the default values provided in
+     * the configuration definition.
+     * Configuration name is optional - if not given via {@link #name(String)} method, it is derived from the annotation interface class name.
      * @param clazz Class that can be adapted from a {@link org.apache.sling.api.resource.Resource}
      * @param <T> Annotation class type
      * @return True/False based on configuration resource node existence in JCR.
